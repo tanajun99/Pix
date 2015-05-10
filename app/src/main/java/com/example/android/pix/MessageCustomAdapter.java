90144c0f9c7b8794ec7dacdd97ed0209b2b45918
Junya Tanaka <tanajun.99@gmail.com>
2015-05-10 17:44:05 +0900

    とりあえず動くようにした

---
+++ app/src/main/java/com/example/android/pix/MessageCustomAdapter.java
@@ -0,0 +1,68 @@
+package com.example.android.pix;
+
+/**
+ * Created by tanakajunnari on 5/9/15.
+ */
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.parse.ParseObject;
+
+public class MessageCustomAdapter extends ArrayAdapter<ParseObject> {
+
+    protected Context mContext;
+    protected List<ParseObject> mMessages;
+
+    public MessageCustomAdapter(Context context, List<ParseObject> messages) {
+        super(context, R.layout.message_item, messages);
+        mContext = context;
+        mMessages = messages;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.message_item, null);
+            holder = new ViewHolder();
+            holder.iconImageView = (ImageView)convertView.findViewById(R.id.messageIcon);
+            holder.nameLabel = (TextView)convertView.findViewById(R.id.senderLabel);
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder)convertView.getTag();
+        }
+
+        ParseObject message = mMessages.get(position);
+
+        if (message.getString(ParseConstants.KEY_FILE_TYPE).equals(ParseConstants.TYPE_IMAGE)) {
+            holder.iconImageView.setImageResource(R.mipmap.ic_action_picture);
+        }
+        else {
+            holder.iconImageView.setImageResource(R.mipmap.ic_action_play_over_video);
+        }
+        holder.nameLabel.setText(message.getString(ParseConstants.KEY_SENDER_NAME));
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        ImageView iconImageView;
+        TextView nameLabel;
+    }
+
+    public void refill(List<ParseObject> messages) {
+        mMessages.clear();
+        mMessages.addAll(messages);
+        notifyDataSetChanged();
+    }
+}
+
---
+++ app/src/main/java/com/example/android/pix/ParseConstants.java
@@ -0,0 +1,22 @@
+package com.example.android.pix;
+
+/**
+ * Created by tanakajunnari on 5/9/15.
+ */
+public final class ParseConstants {
+    // Class name
+    public static final String CLASS_MESSAGES = "Messages";
+
+    // Field names
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_FRIENDS_RELATION = "friendsRelation";
+    public static final String KEY_RECIPIENT_IDS = "recipientIds";
+    public static final String KEY_SENDER_ID = "senderId";
+    public static final String KEY_SENDER_NAME = "senderName";
+    public static final String KEY_FILE = "file";
+    public static final String KEY_FILE_TYPE = "fileType";
+    public static final String KEY_CREATED_AT = "createdAt";
+
+    public static final String TYPE_IMAGE = "image";
+    public static final String TYPE_VIDEO = "video";
+}
