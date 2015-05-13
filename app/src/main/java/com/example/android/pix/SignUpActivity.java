390aeba105bf2d1c02aa827f8be50c43bb7eee84
Junya Tanaka <tanajun.99@gmail.com>
2015-05-13 12:56:19 +0900

    signupのバグ修正

--- app/src/main/java/com/example/android/pix/SignUpActivity.java
+++ app/src/main/java/com/example/android/pix/SignUpActivity.java
@@ -21,6 +21,7 @@ public class SignUpActivity extends Activity {
     protected EditText mPassword;
     protected EditText mEmail;
     protected Button mSignUpButton;
+    protected Button mCancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +35,16 @@ public class SignUpActivity extends Activity {
         mUsername = (EditText)findViewById(R.id.usernameField);
         mPassword = (EditText)findViewById(R.id.passwordField);
         mEmail = (EditText)findViewById(R.id.emailField);
+
+        mCancelButton = (Button)findViewById(R.id.cancel_buttom);
+        mCancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
         mSignUpButton = (Button)findViewById(R.id.signupButton);
         mSignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
